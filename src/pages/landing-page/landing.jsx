31c0ba0b8d192a96c95edfbb28749c9d36590fcf
Kamau Bowie <k.maumau11@gmail.com>
2022-11-27 00:22:21 -0600

    add sidebar to app

--- src/pages/landing-page/landing.jsx
+++ src/pages/landing-page/landing.jsx
@@ -1,6 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/header";
 const Landing = () => {
     return (
-        <div>Landing</div>
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="Landing Page" subtitle="Welcome to the landing page" />
+            </Box>
+        </Box>
     )
 }
 
